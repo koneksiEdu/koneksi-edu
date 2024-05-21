769c0a2ca76d3e8a5a32b0f4b394acafa99fe6b3
koneksiEdu <sadataqwim@gmail.com>
2024-05-21 16:36:47 +0700

    [ KEAMANAN ] Generate token & key, bikin env var

--- koneksi-edu-web/src/lib/supabaseClient.js
+++ koneksi-edu-web/src/lib/supabaseClient.js
@@ -1,3 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient('https://lkyubyoimdryxsrpsbli.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxreXVieW9pbWRyeXhzcnBzYmxpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU4MDM3MTEsImV4cCI6MjAzMTM3OTcxMX0.Mw4utga_HzXIYXeCs-Z37_EiCJ3kjSqL-5oaTLXjAyI')
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
