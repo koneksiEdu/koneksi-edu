91f7d1f3dc70b068aea8c93564c9ebf2ae82767d
koneksiEdu <sadataqwim@gmail.com>
2024-07-19 23:31:16 +0700

    [ PENYESUAIAN ] Penyesuaian pra-submit

--- koneksi-edu-web/src/main.js
+++ koneksi-edu-web/src/main.js
@@ -1,6 +1,7 @@
 import './assets/main.css'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'animate.css';
+import L from 'leaflet';
 import "leaflet/dist/leaflet.css"
 
 import { createApp } from 'vue'
@@ -9,9 +10,20 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: '/marker-icon-2x.png',
+  iconUrl: '/marker-icon.png',
+  shadowUrl: '/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41] 
+});
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
 
 app.mount('#app')
